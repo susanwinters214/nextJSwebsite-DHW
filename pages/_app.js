c4631d04df41d4d51009c129ba63f2c70ad100ae
Susan Winters <susanwinters214@gmail.com>
2022-12-31 10:03:33 -0600

    updated for error fixes

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <meta name="googlebot" content="noindex"></meta>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-Equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport"  content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <link rel="icon" href="images/favicon.ico" type="image/x-icon"></link>
         
